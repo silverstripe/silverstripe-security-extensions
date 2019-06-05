68f537cab954d2526dab789562a8f32701c7de62
Robbie Averill <robbie@silverstripe.com>
2019-06-05 15:05:56 +1200

    Automatically enable sudo mode when logging in

---
+++ src/Extension/SudoModeOnLoginExtension.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace SilverStripe\SecurityExtensions\Extension;
+
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Core\Extension;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\SecurityExtensions\Service\SudoModeServiceInterface;
+
+class SudoModeOnLoginExtension extends Extension
+{
+    /**
+     * Activates sudo mode on a successful login
+     */
+    public function afterLogin(): void
+    {
+        if (Injector::inst()->has(HTTPRequest::class)) {
+            /** @var SudoModeServiceInterface $service */
+            $service = Injector::inst()->get(SudoModeServiceInterface::class);
+
+            /** @var HTTPRequest $request */
+            $request = Injector::inst()->get(HTTPRequest::class);
+
+            $service->activate($request->getSession());
+        }
+    }
+}
